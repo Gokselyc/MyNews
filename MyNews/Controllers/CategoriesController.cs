03fb5a99f99fdca796358c2d2e0da8383642a067
Admin <Admin@DESKTOP-KGJ5C84>
2017-12-15 16:16:43 +0300

    update

--- MyNews/Controllers/CategoriesController.cs
+++ MyNews/Controllers/CategoriesController.cs
@@ -21,15 +21,19 @@ namespace MyNews.Controllers
             this.context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int? Id)
         {
-            var news = context.News.Include(i => i.Category).Where(n => n.IsPublished == true)
+            var news = context.News.Include(i => i.Category).Where(n=>(Id!=null? n.Category.Id==Id:true) && n.IsPublished == true)
                             .OrderByDescending(o => o.PublishDate).Take(10).ToList();
 
             var categories = context.Categories.OrderBy(c => c.Name).Select(s => new CategoryViewModel { Id = s.Id, Name = s.Name, Count = s.News.Count }).ToList();
 
             ViewBag.Categories = categories;
 
+            if (Id!=null)
+            {
+                ViewBag.ActiveCategory = context.Categories.FirstOrDefault(c => c.Id == Id);
+            }
             return View(news);
         }
     }
